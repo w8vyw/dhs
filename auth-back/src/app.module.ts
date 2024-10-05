4fd46947f5fef172d6ef051678914d7baa3c492a
Your Name <w8vyw@ya.ru>
2024-10-05 13:05:19 +0300

    f

--- auth-back/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { AuthModule } from './auth/auth.module'
-
-@Module({
-	imports: [ConfigModule.forRoot(), AuthModule],
-})
-export class AppModule {}
